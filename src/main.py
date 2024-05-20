30032dca4ca00ac23b411b80e01a2d425d3f92b8
Yash Bhalchandra <yashb1004@gmail.com>
2024-05-20 19:37:23 +0530

    added function to get ingredients based on recipe title

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 import json
 
-def load_read_recipe_list():
+def load_recipe_list():
     with open('recipes.json') as recipe_file:
-        parsed_recipe_list = json.load(recipe_file)
-    return parsed_recipe_list
+        recipe_list = json.load(recipe_file)
+    return recipe_list['recipes']
+
+
+def get_ingredients(recipe_title):
+    for recipe in load_recipe_list():
+        if recipe.get('title') == recipe_title:
+            return recipe.get('ingredients')
+
+
