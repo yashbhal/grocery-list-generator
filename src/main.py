4cfce6fc2ac97b307e47ab184cf9fa4b60e89639
Yash Bhalchandra <yashb1004@gmail.com>
2024-05-21 20:20:05 +0530

    Users can get ingredients based on recipe number instead of title

--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ def load_recipe_list():
     return recipe_list['recipes']
 
 
-def get_ingredients(recipe_title, recipes_list):
+def get_ingredients(recipe_number, recipes_list):
     for recipe in load_recipe_list():
-        if recipe.get('title') == recipe_title:
+        if recipe.get('number') == recipe_number:
             return recipe.get('ingredients')
 
 
@@ -21,7 +21,7 @@ def print_recipes(recipes_list):
 def main():
     recipes_list = load_recipe_list()
     print_recipes(recipes_list)
-    recipe_choice = input("Enter the name of the recipe you want to cook: ")
+    recipe_choice = input("Enter the number of the recipe you want to cook: ")
     ingredients = get_ingredients(recipe_choice, recipes_list)
     print(ingredients)
 
