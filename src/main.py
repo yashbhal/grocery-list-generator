3e78b06a8567a3b76d694a7686aa8e141c1db71c
Yash Bhalchandra <yashb1004@gmail.com>
2024-06-01 14:36:53 +0530

    refactor for testability

--- src/main.py
+++ src/main.py
@@ -14,47 +14,56 @@ def get_ingredients(recipe_number, recipes_list):
 
 
 def print_recipes(recipes_list):
-    print('\nRecipe menu\n')
+    output = '\nRecipe menu\n'
     for recipe in recipes_list:
-        print(f"{recipe.get('number')}. {recipe.get('title')}")
+        output += f"\n{ + recipe.get('number')}. {recipe.get('title')}"
+    print(output)
+    return output
     
 
 def validate_integer_input(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            return value
-        except ValueError:
-            print("Invalid input. Enter a valid integer.")
+    try:
+        value = int(input(prompt))
+        return value
+    except ValueError:
+        raise ValueError("Invalid input. Enter a valid integer.")
 
 
 def validate_choice(prompt, max_value):
-    while True:
-        value = validate_integer_input(prompt)
-        if 1 <= value <= max_value:
-            return value
-        else:
-            print(f"Invalid recipe choice. Enter a number between 1 and {max_value}")
+    value = validate_integer_input(prompt)
+    if 1 <= value <= max_value:
+        return value
+    else:
+        raise ValueError(f"Invalid recipe choice. Enter a number between 1 and {max_value}")
 
 
-def main():
+def get_weeks_ingredients(recipe_choices, recipes_list):
     weeks_ingredients = set()
-    recipes_list = load_recipe_list()
-    print_recipes(recipes_list)
-    recipe_amount = validate_choice("How many recipes would you like to prep for the week? ", len(recipes_list))
-
-    for _ in range(recipe_amount):
-        recipe_choice = validate_choice("Enter the number of the recipe you want to cook: ", len(recipes_list))
+    for recipe_choice in recipe_choices:        
         ingredients = get_ingredients(recipe_choice, recipes_list)
         if ingredients:
             weeks_ingredients.update(get_ingredients(recipe_choice, recipes_list))
         else:
-            print(f"No ingredients found for {recipe_choice}.")
+            raise ValueError(f"No ingredients found for {recipe_choice}.")
+    return list(weeks_ingredients)
 
-    ingredients_list = list(weeks_ingredients)
-    print("\nIngredients for the week:: ")
-    for ingredient in ingredients_list:
-        print(f"- {ingredient}")
+
+def main():
+    recipes_list = load_recipe_list()
+    print_recipes(recipes_list)
+    recipe_amount = validate_choice("How many recipes would you like to prep for the week? ", len(recipes_list))
+    recipe_choices = []
+
+    for _ in range(recipe_amount):
+        recipe_choice = validate_choice("Enter the number of the recipe you want to cook: ", len(recipes_list))
+        recipe_choices.append(recipe_choice)
+    try:
+        weeks_ingredients = get_weeks_ingredients(recipe_choices, recipes_list)
+        print("\nIngredients for the week:")
+        for ingredient in weeks_ingredients:
+            print(f"- {ingredient}")
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == "__main__":
